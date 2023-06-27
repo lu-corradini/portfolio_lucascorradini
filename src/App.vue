72dfb6eb680e248644fd651690badd06602a2d2b
lu-corradini <lucas.corradini.2003@gmail.com>
2023-06-27 16:37:08 -0700

    testing

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <NavMenu/>
+  </div>
+</template>
+
+<script>
+import NavMenu from './components/NavMenu.vue';
+
+export default {
+  name: 'App',
+  components: {
+    NavMenu
+  }
+}
+</script>
+
+<style>
+  @import url("./scss/style.scss");
+</style>
---
+++ src/components/NavMenu.vue
@@ -0,0 +1,44 @@
+<template>
+    <nav>
+        <section class="simbol">
+            <a href="#">
+                 <i class="fa-regular fa-l"></i>
+            </a>
+        </section>
+        <h1>
+            Web Developer
+        </h1>
+        <section class="menu">
+            <aside>
+                <ul>
+                    <li>
+                        <a href="#"> <i class="fa-regular fa-address-card"></i> About</a>
+                    </li>
+                    <li>
+                        <a href="#"> <i class="fa-regular fa-address-book"></i> Contact</a>
+                    </li>
+                    <li>
+                        <a href="#"> <i class="fa-solid fa-laptop-file"></i> Projects</a>
+                    </li>
+                </ul>
+            </aside>
+            <article>
+                <a href="#">Resume</a>
+            </article>
+            <details>
+                <summary class="fa-solid fa-bars"></summary>
+                <ul>
+                    <li><a href="#">About Me</a></li>
+                    <li><a href="#">Contact Me</a></li>
+                    <li><a href="#">Projects</a></li>
+                    <li><a href="#">Resume</a></li>
+                </ul>
+            </details>
+        </section>
+    </nav>
+</template>
+
+
+<style>
+
+</style>
