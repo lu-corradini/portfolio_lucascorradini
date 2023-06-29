ef034681a5272b4894d3ac4224c806465d7a674c
lu-corradini <lucas.corradini.2003@gmail.com>
2023-06-28 19:09:35 -0700

    doing about

--- src/components/AboutPage.vue
+++ src/components/AboutPage.vue
@@ -1,7 +1,28 @@
 <template>
     <main class="aboutMain">
         <section class="first">
-            
+            <section class="left">
+                <article>
+                    <h2>
+                        About
+                    </h2>
+                    <p class="title">
+                        Who is Corradini?
+                    </p>
+                </article>
+                <aside>
+                    <p>
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Incidunt magnam possimus minus distinctio aperiam id ipsum atque commodi dolor amet pariatur accusantium temporibus eligendi vero, quidem vel quisquam, qui natus sapiente ex ad repudiandae laboriosam culpa! Laboriosam nulla in repellendus.
+                    </p>
+                </aside>
+            </section>
+            <img src="./../assets/pexels-lil-artsy-1111597.jpg" alt="pi-01">
+        </section>
+        <section class="second">
+
+        </section>
+        <section class="third">
+
         </section>
     </main>
 </template>
